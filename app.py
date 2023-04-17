971e0bb7e9940f185a09d16d8b13c4aeb37952cf
nikita <n.shavkin@it-park.tech>
2023-04-17 16:40:56 +0300

    HW_23

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
 
@@ -8,10 +8,41 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
+def comnad_function(com, value, file_name):
+    if com == 'filter':
+        result = list(filter(lambda s: value in s, file_name))
+    if com == 'map':
+        result = "\n".join(map(lambda s: s.split()[int(value) + 1], file_name))
+    if com == 'limit':
+        result = list(file_name)[:int(value)]
+    if com == 'unique':
+        result = list(set(file_name))
+    if com == 'sort':
+        is_rev = value == 'desc'
+        result = sorted(file_name, reverse=is_rev)
+    return result
+
+
 @app.route("/perform_query")
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    file_name = request.json['file_name']
+    com1 = request.json['com1']
+    value1 = request.json['value1']
+    com2 = request.json['com2']
+    value2 = request.json['value2']
+    if file_name is None or com1 is None or value1 is None:
+        abort(400, "Неверный запрос")
+    is_file = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(is_file):
+        abort(400, 'file not found')
+    with open(is_file, 'r') as file:
+        result = comnad_function(com1, value1, file)
+        if com2 and value2:
+            result = comnad_function(com2, value2, result)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+
