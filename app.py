e2adf50182e8d57fba9df1b791d77699e82e41b8
nikita <n.shavkin@it-park.tech>
2023-04-20 21:08:07 +0300

    24

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-
+import re
+from typing import List, Union, TextIO, Optional
 from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
@@ -8,35 +9,37 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def comnad_function(com, value, file_name):
+def comnad_function(com: str, value: str, file_name: TextIO) -> Union[str, List]:
     if com == 'filter':
-        result = list(filter(lambda s: value in s, file_name))
+        return list(filter(lambda s: value in s, file_name))
     if com == 'map':
-        result = "\n".join(map(lambda s: s.split()[int(value) + 1], file_name))
+        return "\n".join(map(lambda s: s.split()[int(value) + 1], file_name))
     if com == 'limit':
-        result = list(file_name)[:int(value)]
+        return list(file_name)[:int(value)]
     if com == 'unique':
-        result = list(set(file_name))
+        return list(set(file_name))
     if com == 'sort':
         is_rev = value == 'desc'
-        result = sorted(file_name, reverse=is_rev)
-    return result
+        return sorted(file_name, reverse=is_rev)
+    if com == 'regex':
+        regex: re.Pattern = re.compile(value)
+        return list(filter(lambda s: regex.findall(s), file_name))
 
 
 @app.route("/perform_query")
 def perform_query():
-    file_name = request.json['file_name']
-    com1 = request.json['com1']
-    value1 = request.json['value1']
-    com2 = request.json['com2']
-    value2 = request.json['value2']
+    file_name: Optional[str] = request.json['file_name']
+    com1: Optional[str] = request.json['com1']
+    value1: Optional[str] = request.json['value1']
+    com2: Optional[str] = request.json['com2']
+    value2: Optional[str] = request.json['value2']
     if file_name is None or com1 is None or value1 is None:
         abort(400, "Неверный запрос")
     is_file = os.path.join(DATA_DIR, file_name)
     if not os.path.exists(is_file):
         abort(400, 'file not found')
     with open(is_file, 'r') as file:
-        result = comnad_function(com1, value1, file)
+        result: Union[List, str, TextIO] = comnad_function(com1, value1, file)
         if com2 and value2:
             result = comnad_function(com2, value2, result)
     return jsonify(result)
@@ -44,5 +47,3 @@ def perform_query():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-
-
